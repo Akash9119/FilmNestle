c840b9014655a0ae16095dbbb2465eb91762d049
Akash <akashj.vasava@gmail.com>
2023-08-04 18:16:15 +0530

    Fixed API Security Bug

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { HiOutlineSearch } from 'react-icons/hi';
+import { SlMenu } from 'react-icons/sl';
+import { VcChromeClose } from 'react-icons/vsc';
+import { useNavigate, useLocation } from 'react-router-dom';
+
 import './style.scss';
 
+import ContentWrapper from '../contentWrapper/ContentWrapper';
+import logo from '../../assets/FilmNestle.png';
+
 const Header = () => {
+  const [show, setShow] = useState("top");
+  const [lastScrollY, setLastScrollY] = useState(0);
+  const [mobileMenu, setMobileMenu] = useState(false);
+  const [query, setQuery] = useState("");
+  const [showSearch, setShowSearch] = useState("");
+  const navigate = useNavigate();
+  const location = useLocation();
+
   return (
     <div>
-      <h1>Header</h1>
+      Header
     </div>
   )
 }
 
-export default Header
+export default Header;
