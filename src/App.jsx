0702602dc6fe3fbd8d73d01282801003f8b19691
Akash <akashj.vasava@gmail.com>
2023-08-06 10:33:12 +0530

    Changed Logo Image and removed console.log

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,6 @@ import PageNotFound from "./pages/404/PageNotFound";
 function App() {
     const dispatch = useDispatch();
     const { url } = useSelector((state) => state.home);
-    console.log(url);
 
     useEffect(() => {
         fetchApiConfig();
@@ -25,7 +24,6 @@ function App() {
 
     const fetchApiConfig = () => {
         fetchDataFromApi("/configuration").then((res) => {
-            console.log(res);
 
             const url = {
                 backdrop: res.images.secure_base_url + "original",
@@ -47,7 +45,6 @@ function App() {
         });
 
         const data = await Promise.all(promises);
-        console.log(data);
         data.map(({ genres }) => {
             return genres.map((item) => (allGenres[item.id] = item));
         });
